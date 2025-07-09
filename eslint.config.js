3594100d9a5a0707d7f8a8d2f9195a286b6d6f34
ree-jp <mail@ree-jp.net>
2025-07-09 21:48:16 +0900

    💚 ESLintの設定が悪かった

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,10 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    files: ["**/*.{jsx,tsx}"],
+  },
   ...eslintPluginAstro.configs.recommended,
   {
     rules: {
