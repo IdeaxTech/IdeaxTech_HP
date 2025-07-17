1c74a64c3eca2e7e69408cbd7f2e4b4ebb3cd4eb
Chomaru <146067661+Chocir@users.noreply.github.com>
2025-07-17 15:45:41 +0900

    prettierの導入・github actionsのジョブの追加？

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import eslintPluginAstro from "eslint-plugin-astro";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   tseslint.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
-    files: ["**/*.{jsx,tsx}"],
+    files: ['**/*.{jsx,tsx}'],
   },
   ...eslintPluginAstro.configs.recommended,
   {
     rules: {
       'astro/no-set-html-directive': 'error',
     },
-  }
+  },
 ]);
